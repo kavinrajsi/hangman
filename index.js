473c952748771752d35c2e113f2e50c787b55d56
kavinrajsi <we@sikavinraj.me>
2020-07-25 15:37:04 +0530

    rough layout for function are added

--- index.js
+++ index.js
@@ -15,32 +15,46 @@ function createButton() {
 
     document.getElementById('keyboard').innerHTML = buttonsTag;
 }
+
 // guessed word
 function myGuess(letter) {
     if(guessed.indexOf(letter) !== -1){
-        // console.log('if : ' + guessed);
     } else{
         guessed.push(letter);
         checkWord(guessed);
-        // console.log('else : ' + guessed);
     }
 }
 // check the word
 function checkWord(guessed) {
-    // console.log('check word : ' + typeof guessed);
-    console.log( guessed.stringify);
+    console.log(guessed.join(''));
+    if (guessMe == guessed.join('')) {
+        gameWon();
+    } else {
+        gameLoss();
+    }
 }
+
 function gameWon() {
-    alert('game won');
+    console.log('game won');
 }
-function gameLoss(){
-    alert('game loss');
+function gameLoss() {
+    const count = mistakes++;
+    console.log('game loss: ' + mistakes);
+    updatePicture();
+    document.getElementById('mistakes').innerHTML = count+1;
+    if (count+1 === maxWrong) {
+        document.getElementById('keyboard').innerHTML = 'you dead, please refresh the bowser';
+    }
+}
+function updatePicture() {
+    document.getElementById('manPic').src = './images/' + mistakes + '.jpg';
 }
 // reset
 function reset() {
     mistakes = 0;
     guessed = [];
     document.getElementById('manPic').src = './images/0.jpg';
+    createButton();
 }
 
 // function calling
